8b0c9aad713b3e67a2195285de53f10d86d9e813
Antonio del Caño <antonio@kasfactory.net>
2015-07-24 13:33:19 +0200

    Admin improvement

--- athletes/admin.py
+++ athletes/admin.py
@@ -1,6 +1,20 @@
 from athletes.models import Athlete, ClubForAthlete
 from django.contrib import admin
+from races.models import Result
+
+
+class ResultInline(admin.TabularInline):
+    model = Result
+    fields = ('position', 'record', 'extra')
+    readonly_fields = ('position', 'record')
+    extra = 1
+
+class AthleteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname')
+    search_fields = ('name', 'surname')
+    inlines = (ResultInline,)
+
 
 # Register your models here.
-admin.site.register(Athlete)
+admin.site.register(Athlete, AthleteAdmin)
 admin.site.register(ClubForAthlete)
